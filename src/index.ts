17b300075effd313f2f456e801ab93b4a115a819
devin jhu <devin.jhu@mths.ca>
2022-09-28 01:47:55 +0000

    code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 /**
- * The HelloWorld program implements an application that
- * simply displays "Hello World!" to the standard output.
+ * the truck weight program
  *
  * By:      Devin Jhu
  * Version: 1.0
- * Since:   2020-01-01
+ * Since:   2022 09 27
  */
-console.log('Hello, World!')
-console.log('\ndone.')
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+// let logSize: number
+const logWeight = 20
+const maxTruckWeight = 1100
+const inputString = prompt('Enter log length (0.25, 0.5, or 1) (M): ')
+const logLenght = parseFloat(inputString)
+const totalWeight = logLenght * logWeight
+const logNumber = maxTruckWeight / totalWeight
+console.log(` ${logNumber} logs will fit`)
+
+console.log('\nDone.')
